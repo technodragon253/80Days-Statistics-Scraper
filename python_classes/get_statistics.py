205bbcb679251322291adb4f74a3ea7bb2a1a78a
Luke <lukemcgranahan@gmail.com>
2021-11-24 12:37:58 -0600

    Partially refactored "get_statistics.py"

--- python_classes/get_statistics.py
+++ python_classes/get_statistics.py
@@ -1,23 +1,25 @@
 import json
-from time import time
-from typing import List
 from python_classes.constants import colors, team_lookup_table
 from os import name, system
 import python_classes.data_types as data_types
 from python_classes.data_types import Payment, Sabotage
 
+#Short function for gammer stuff.
 def s(i):
     if i != 1:
         return "s"
     else:
         return ""
 
-def p(player_lookup_table, player_id):
+#Short function for player name look ups.
+player_lookup_table = {}
+def p(player_id):
     try:
         return player_lookup_table[player_id]
     except:
         return str(player_id)
 
+#Short function for team name look up.
 def t(team_id):
     try:
         return team_lookup_table[team_id]
@@ -46,7 +48,6 @@ What statistic would you like to get?
 {colors.green}(Any other key to exit){colors.default}
 """)
 #Get the player lookup table
-player_lookup_table = {}
 for game in data:
         for team in game.teams:
             for player in team.players:
@@ -60,43 +61,90 @@ if i == "1":
 Enter the player id or name of the player you want to look up.
 """)
     system('cls' if name == 'nt' else 'clear')
-    #Find the games whrere the player did things.
+    #Find all the info we need from the data.
     player_id = ""
     player_name = ""
-    games_to_scan = []
     players_sabotages = []
     players_payments = []
+    total_wins = 0
+    total_games = 0
+    win_rate = 0
+    times_boars = 0
+    times_wolves = 0
+    times_stallions = 0
     for game in data:
         for team in game.teams:
-            for player in team.players:
-                if player_id != "": #If the player id has been found,
-                    if player.id == player_id: #Check if the player were looking for is the player we're iterating over.
-                        games_to_scan.append(game)
-                elif player.id == i:
-                    games_to_scan.append(game)
-                    player_id = player.id
-                    player_name = player.name
-                elif player.name.lower() == i.lower(): #If the name of a player was entered change 'i' to be the player id.
-                    i = player.id
-                    games_to_scan.append(game)
-                    player_id = player.id
-                    player_name = player.name
-    if games_to_scan == []:
-        raise Exception("Player did nothing!")
-
-    print(f"""{colors.purple}
-Statistics for {player_name} with id of {player_id}:
-    {colors.default}""")
-
-    #Find all sabotages/payments by the player.
-    for game in games_to_scan:
-        for team in game.teams:
+            #Find player sabotages.
             for sabotage in team.sabotages:
-                if sabotage.player_id == i:
+                if player_id != "": #If the player id has been found,
+                    if sabotage.player_id == player_id: #Check if the player were looking for is the player we're iterating over.
+                        players_sabotages.append(sabotage)
+                elif sabotage.player_id == i:
                     players_sabotages.append(sabotage)
+                    player_id = sabotage.player_id
+                    player_name = p(sabotage.player_id)
+                elif p(sabotage.player_id).lower() == i.lower(): #If the name of a player was entered change 'i' to be the player id.
+                    players_sabotages.append(sabotage)
+                    player_id = sabotage.player_id
+                    player_name = p(sabotage.player_id)
+            #Find player payments.
             for payment in team.payments:
-                if payment.player_id == i:
+                if player_id != "": #If the player id has been found,
+                    if payment.player_id == player_id: #Check if the player were looking for is the player we're iterating over.
+                        players_payments.append(payment)
+                elif payment.player_id == i:
                     players_payments.append(payment)
+                    player_id = payment.player_id
+                    player_name = p(payment.player_id)
+                elif p(payment.player_id).lower() == i.lower(): #If the name of a player was entered change 'i' to be the player id.
+                    players_payments.append(payment)
+                    player_id = payment.player_id
+                    player_name = p(payment.player_id)
+                    
+            #Find the teams the player was on.
+            for player in team.players:
+                if player_id != "":
+                    if player.id == player_id:
+                        total_games += 1
+                        if team.place == 1:
+                            total_wins += 1
+                        if team.id == 1:
+                            times_boars += 1
+                        elif team.id == 2:
+                            times_wolves += 1
+                        elif team.id == 3:
+                            times_stallions += 1
+                elif player.id == i:
+                    player_id = player.id
+                    player_name = player.name
+                    if player.id == player_id:
+                        total_games += 1
+                        if team.place == 1:
+                            total_wins += 1
+                        if team.id == 1:
+                            times_boars += 1
+                        elif team.id == 2:
+                            times_wolves += 1
+                        elif team.id == 3:
+                            times_stallions += 1
+                elif player.name.lower() == i.lower():
+                    player_id = player.id
+                    player_name = player.name
+                    if player.id == player_id:
+                        total_games += 1
+                        if team.place == 1:
+                            total_wins += 1
+                        if team.id == 1:
+                            times_boars += 1
+                        elif team.id == 2:
+                            times_wolves += 1
+                        elif team.id == 3:
+                            times_stallions += 1
+    if total_games == 0:
+        print(f"{colors.red}Player, '{i}', hasn't played any games!{colors.default}")
+        input("\nPress Enter to Exit...")
+        quit()
+    win_rate = (total_wins / total_games) * 100
 
     #Find payment stats.
     total_payed = 0
@@ -126,22 +174,11 @@ Statistics for {player_name} with id of {player_id}:
     most_payed_location = max(payed_locations, key=payed_locations.get)
     most_often_location = max(often_locations, key=often_locations.get)
 
-    print(f"""{colors.dark_green}
-Payment stats:
-{colors.green}{total_payed} lifetime coin{s(total_payed)} payed.
-{number_of_payments} indivival payment{s(number_of_payments)}.
-Biggest payment: {max_payment} coin{s(max_payment)}.
-Smallest payment: {min_payment} coin{s(min_payment)}.
-Average payment: {average_payment} coin{s(average_payment)}.
-Lifetime highest payed for location: {most_payed_location} with {payed_locations[most_payed_location]} coin{s(payed_locations[most_payed_location])} payed!
-Lifetime most often payed for location: {most_often_location} traveled to {often_locations[most_often_location]} time{s(often_locations[most_often_location])}!
-{colors.default}""")
-
     #Find sabotage stats.
     total_sabotaged = 0
     number_of_sabotages = 0
     max_sabotage = 0
-    min_sabotage = 100000 #Need to set it to an obsured number so the min function works.
+    min_sabotage = 100000 #Need to set it to an absurd number so the min function works.
     average_payment = 0
     sabotaged_locations = {}
     most_sabotaged_location = ""
@@ -165,7 +202,21 @@ Lifetime most often payed for location: {most_often_location} traveled to {often
     most_sabotaged_location = max(sabotaged_locations, key=sabotaged_locations.get)
     highest_sabotaged_location = max(sabotaged_locations, key=sabotaged_locations.get)
 
-    print(f"""{colors.dark_red}
+    #Print way too many statistics!
+    print(f"""{colors.purple}
+Statistics for {player_name} with id of {player_id}:
+
+{colors.dark_green}
+Payment stats:
+{colors.green}{total_payed} lifetime coin{s(total_payed)} payed.
+{number_of_payments} indivival payment{s(number_of_payments)}.
+Biggest payment: {max_payment} coin{s(max_payment)}.
+Smallest payment: {min_payment} coin{s(min_payment)}.
+Average payment: {average_payment} coin{s(average_payment)}.
+Lifetime highest payed for location: {most_payed_location} with {payed_locations[most_payed_location]} coin{s(payed_locations[most_payed_location])} payed!
+Lifetime most often payed for location: {most_often_location} traveled to {often_locations[most_often_location]} time{s(often_locations[most_often_location])}!
+
+{colors.dark_red}
 Sabotages stats:
 {colors.red}{total_sabotaged} lifetime coin{s(total_sabotaged)} sabotaged.
 {number_of_sabotages} indivival sabotages{s(number_of_sabotages)}.
@@ -174,43 +225,18 @@ Smallest sabotage: {min_sabotage} coin{s(min_sabotage)}.
 Average sabotage: {average_payment} coin{s(average_payment)}.
 Lifetime highest total sabotaged location: {highest_sabotaged_location} with {highest_locations[highest_sabotaged_location]} coin{s(highest_locations[highest_sabotaged_location])} sabotaged!
 Lifetime most often sabotaged location: {most_sabotaged_location} sabotaged {sabotaged_locations[most_sabotaged_location]} time{s(sabotaged_locations[most_sabotaged_location])}!
-{colors.default}""")
 
-    #Find win and team stats
-    total_wins = 0
-    total_games = 0
-    win_rate = 0
-    times_boars = 0
-    times_wolves = 0
-    times_stallions = 0
-
-    for game in games_to_scan:
-        for iter, team in enumerate(game.teams):
-            for player in team.players:
-                if player.id == i:
-                    total_games += 1
-                    if team.place == 1:
-                        total_wins += 1
-                    if iter == 0:
-                        times_boars += 1
-                    elif iter == 1:
-                        times_wolves += 1
-                    elif iter == 2:
-                        times_stallions += 1
-    
-    win_rate = (total_wins / total_games) * 100
-
-    print(f"""{colors.dark_blue}
+{colors.dark_yellow}
 Win stats:
-{colors.blue}{total_wins} total win{s(total_wins)}.
+{colors.yellow}{total_wins} total win{s(total_wins)}.
 {total_games} total game{s(total_games)}.
 {win_rate}% win rate.
 
-{colors.dark_yellow}
+{colors.dark_blue}
 Team stats:
-{colors.yellow}Player was on the {colors.default}Argent Boars{colors.yellow} {times_boars} time{s(times_boars)}!
-Player was on the {colors.cyan}Azure Wolves{colors.yellow} {times_wolves} time{s(times_wolves)}!
-Player was on the {colors.red}Crimson Stallions{colors.yellow} {times_stallions} time{s(times_stallions)}!
+{colors.blue}Player was on the {colors.default}Argent Boars{colors.blue} {times_boars} time{s(times_boars)}!
+Player was on the {colors.cyan}Azure Wolves{colors.blue} {times_wolves} time{s(times_wolves)}!
+Player was on the {colors.red}Crimson Stallions{colors.blue} {times_stallions} time{s(times_stallions)}!
 {colors.default}""")
 
 ################################################################
@@ -269,11 +295,11 @@ elif i == "3":
 
     print(f"""{colors.dark_green}
 Payment Hall of Fame:
-{colors.green}Largest payment: {highest_payment.amount} coin{s(highest_payment.amount)} by {p(player_lookup_table, highest_payment.player_id)} toward {highest_payment.location} at {highest_payment.time.strftime("%H:%M %b %d %Y")}!{colors.default}
-{colors.green}Highest lifetime coins payed: {p(player_lookup_table, highest_lifetime_payment.player_id)} with {highest_lifetime_payment.amount} lifetime coin{s(highest_lifetime_payment.amount)} payed!{colors.default}
+{colors.green}Largest payment: {highest_payment.amount} coin{s(highest_payment.amount)} by {p(highest_payment.player_id)} toward {highest_payment.location} at {highest_payment.time.strftime("%H:%M %b %d %Y")}!{colors.default}
+{colors.green}Highest lifetime coins payed: {p(highest_lifetime_payment.player_id)} with {highest_lifetime_payment.amount} lifetime coin{s(highest_lifetime_payment.amount)} payed!{colors.default}
 
 {colors.dark_red}
 Sabotage Hall of Fame:
-{colors.red}Largest sabotage: {highest_sabotage.amount} coin{s(highest_sabotage.amount)} by {p(player_lookup_table, highest_sabotage.player_id)} at {highest_sabotage.time.strftime("%H:%M %b %d %Y")} against {t(highest_sabotage.against)} trying to travel to {highest_sabotage.location}!{colors.default}
-{colors.red}Highest lifetime coins sabotaged: {p(player_lookup_table, highest_lifetime_payment.player_id)} with {highest_lifetime_sabotage.amount} lifetime coin{s(highest_lifetime_sabotage.amount)} sabotaged!{colors.default}
+{colors.red}Largest sabotage: {highest_sabotage.amount} coin{s(highest_sabotage.amount)} by {p(highest_sabotage.player_id)} at {highest_sabotage.time.strftime("%H:%M %b %d %Y")} against {t(highest_sabotage.against)} trying to travel to {highest_sabotage.location}!{colors.default}
+{colors.red}Highest lifetime coins sabotaged: {p(highest_lifetime_payment.player_id)} with {highest_lifetime_sabotage.amount} lifetime coin{s(highest_lifetime_sabotage.amount)} sabotaged!{colors.default}
 """)
